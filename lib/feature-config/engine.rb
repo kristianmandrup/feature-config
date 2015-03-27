6bbd963f5fc386f48562c027177cd895efa5d381
ilya shcherbinin <m0n9oose@gmail.com>
2015-03-27 14:25:03 +0300

    initialize features before environment configuration

--- lib/feature-config/engine.rb
+++ lib/feature-config/engine.rb
@@ -1,6 +1,6 @@
 module FeatureConfig
   class Engine < Rails::Engine
-    initializer 'feature-config.helpers' do
+    initializer 'feature-config.helpers', before: :load_environment_config do
       FeatureConfig.initialize_feature_configs
     end
   end
