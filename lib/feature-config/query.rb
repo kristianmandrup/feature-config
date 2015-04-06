9d2885d82e9c77ce70b5623c8f5ba8705b3222e2
ilya shcherbinin <m0n9oose@gmail.com>
2015-04-06 15:27:03 +0300

    Query#initialize: move AR query into separated method

--- lib/feature-config/query.rb
+++ lib/feature-config/query.rb
@@ -4,7 +4,13 @@ module FeatureConfig
 
     def initialize(attributes)
       @attributes = attributes
-      @ids = Betrails::User.send(name, value).pluck(:id)
+      @ids = find_ids
+    end
+
+    private
+
+    def find_ids
+      Betrails::User.send(name, value).pluck(:id)
     end
   end
 end
