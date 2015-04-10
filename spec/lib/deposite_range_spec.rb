477cae4d7a41229273969023f3d017b935be474c
ilya shcherbinin <m0n9oose@gmail.com>
2015-04-10 11:52:00 +0300

    use subject instead of let

--- spec/lib/deposite_range_spec.rb
+++ spec/lib/deposite_range_spec.rb
@@ -2,40 +2,39 @@ require 'spec_helper'
 
 RSpec.describe FeatureConfig::DepositeRange do
   context do
-    subject { FeatureConfig::DepositeRange }
-    let(:deposite_range) { subject.new(query: 'deposite_range', min: 500_000, max: 600_000) }
+    subject { FeatureConfig::DepositeRange.new(query: 'deposite_range', min: 500_000, max: 600_000) }
 
     before do
-      expect_any_instance_of(subject).to receive(:find_ids).and_return([1,2,3])
+      expect_any_instance_of(FeatureConfig::DepositeRange).to receive(:find_ids).and_return([1,2,3])
     end
 
     context '#ids' do
       it 'returns an array' do
-        expect(deposite_range.ids).to be_kind_of(Array)
+        expect(subject.ids).to be_kind_of(Array)
       end
 
       it 'returns ids' do
-        expect(deposite_range.ids).to match_array([1,2,3])
+        expect(subject.ids).to match_array([1,2,3])
       end
     end
 
     context '#name' do
       it 'returns name as string' do
-        expect(deposite_range.send(:name)).to be_kind_of(String)
+        expect(subject.send(:name)).to be_kind_of(String)
       end
 
       it 'returns name of betrails user_query' do
-        expect(deposite_range.send(:name)).to eq('with_deposits')
+        expect(subject.send(:name)).to eq('with_deposits')
       end
     end
 
     context '#value' do
       it 'returns value as range' do
-        expect(deposite_range.send(:value)).to be_kind_of(Range)
+        expect(subject.send(:value)).to be_kind_of(Range)
       end
 
       it 'returns range with correct values' do
-        expect(deposite_range.send(:value)).to eq(500_000..600_000)
+        expect(subject.send(:value)).to eq(500_000..600_000)
       end
     end
   end
