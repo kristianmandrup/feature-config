39c0129e45ca1559df43fe2aeaa000210f7e7cc6
ilya shcherbinin <m0n9oose@gmail.com>
2015-04-13 10:56:16 +0300

    decouple config entries path to separate method
    (cherry picked from commit a26a58dcf9cca596b44009f03bf0f36965ba9064)

--- lib/feature-config/configuration_handler.rb
+++ lib/feature-config/configuration_handler.rb
@@ -5,9 +5,13 @@ module FeatureConfig
     private
 
     def fetch_feature_configs(*path)
-      Dir[Rails.root.join('config', 'features', *path, '*.yml')].inject({}) do |conf, entry|
+      Dir[config_entries_path(path)].inject({}) do |conf, entry|
         conf.update(YAML.load_file(entry))
       end
+    end
+
+    def config_entries_path(path)
+      Rails.root.join('config', 'features', *path, '*.yml')
     end
   end
 end
